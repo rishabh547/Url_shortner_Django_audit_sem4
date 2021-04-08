e2628c2ba93bc21013fcb42e9eb8ed4a7f87c6c2
rishabh541 <rishabhkothari541@gmail.com>
2021-04-08 12:07:22 +0530

    Url shortner project pushed & updated changes

--- urlshortner/urlshortner/urls.py
+++ urlshortner/urlshortner/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from authentication.views import login, signup, logout
+from urlhandler.views import dashboard, generate, home, deleteurl
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('login/', login, name="login"),
+    path('signup/', signup, name="signup"),
+    path('logout/', logout, name="logout"),
+    path('dashboard/', dashboard, name="dashboard"),
+    path('generate/', generate, name="generate"),
+    path('deleteurl/', deleteurl, name="deleteurl"),
+    path('<str:query>/', home, name="home"),
 ]
